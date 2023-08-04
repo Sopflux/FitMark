96dd919e4b387ade4b3b5705a82bfcb7794f14f5
Sop <sopjeong@gmail.com>
2023-08-04 09:34:17 +0900

    초기 커밋

---
+++ FitMark/src/main/java/com/example/demo/FitMarkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitMarkApplication.class, args);
+	}
+
+}
